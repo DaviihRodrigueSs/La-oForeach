60d49cc9889b11eb0aa38ada43789e469af1501b
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-04 18:27:23 -0300

    LaçoForeach (para cada)

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+namespace LaçoForeach
+{
+    class program
+    {
+        //utilizando o foreach
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+
+            }
+            Console.WriteLine("______________________________________");
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
